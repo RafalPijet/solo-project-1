bca246c85cf929c731cdf1c13b4872dc7a6ebcb9
Rafal Pijet <stronglopez@wp.pl>
2019-07-16 17:50:43 +0200

    Change js files structure

--- js/script.js
+++ js/script.js
@@ -1,33 +1,30 @@
 "use strict";
 (function () {
-    var hambMobileButton = document.getElementById("hambMobile");
-    var hambNormalButton = document.getElementById("hamb-normal");
-    var hambShortButton = document.getElementById("hamb-short");
     var verticalMenu = document.getElementById("vartical");
     var workField = document.getElementById("field");
     var menuLeft = document.getElementById("menu-left");
     var check = false;
 
-    const setContainerPosition = function () {
+    document.querySelectorAll("#overlay .js--close-modal").forEach(function (btn) {
+        btn.addEventListener("click", function (e) {
+            e.preventDefault();
+            modalHandling();
+        })
+    });
 
-        if (window.innerWidth <= 992) {
-            workField.classList.remove("normal-margin");
-            workField.classList.add("low-margin");
-        } else if (window.innerWidth > 992 && check == false) {
-            workField.classList.add("normal-margin");
-            workField.classList.remove("low-margin");
-        } else if (window.innerWidth > 992 && check == true) {
-            workField.classList.remove("normal-margin");
-            workField.classList.add("low-margin");
+    document.querySelector("#overlay").addEventListener("click", function (e) {
+        if (e.target === this) {
+            modalHandling();
         }
+    });
 
-        if (window.innerWidth <= 768) {
-            workField.classList.remove("low-margin");
-            workField.classList.remove("container-max-width");
+    document.addEventListener("keyup", function (e) {
+        if (e.keyCode === 27) {
+            modalHandling();
         }
-    };
+    });
 
-    hambMobileButton.addEventListener("click", function (event) {
+    document.getElementById("hambMobile").addEventListener("click", function (event) {
         event.stopPropagation();
         verticalMenu.classList.toggle("vertical-menu-off");
         verticalMenu.classList.toggle("vertical-menu");
@@ -36,7 +33,7 @@
         workField.classList.toggle("top-margin");
     });
 
-    hambNormalButton.addEventListener("click", function (event) {
+    document.getElementById("hamb-normal").addEventListener("click", function (event) {
         event.stopPropagation();
         menuLeft.classList.add("left-menu-off");
         menuLeft.classList.remove("left-menu-on");
@@ -45,13 +42,13 @@
         check = true;
     });
 
-    hambShortButton.addEventListener("click", function (event) {
+    document.getElementById("hamb-short").addEventListener("click", function (event) {
         event.stopPropagation();
         menuLeft.classList.remove("left-menu-off");
         menuLeft.classList.add("left-menu-on");
         check = false;
 
-        if (window.innerWidth > 992 && check == false) {
+        if (window.innerWidth > 992 && check === false) {
 
             workField.classList.add("normal-margin");
             workField.classList.remove("low-margin");
@@ -65,6 +62,30 @@
     window.addEventListener("load", function () {
         setContainerPosition();
     });
+
+    document.getElementById("left-manager").addEventListener("click", function () {
+        modalHandling("#chat-online");
+    });
+
+    document.getElementById("top-manager").addEventListener("click", function () {
+        modalHandling("#chat-online");
+    });
+
+    document.getElementById("left-manager-short").addEventListener("click", function () {
+        modalHandling("#chat-online");
+    });
+
+    document.getElementById("quit-icon").addEventListener("click", function () {
+        modalHandling("#quit");
+    });
+
+    document.getElementById("enter-button").addEventListener("click", function () {
+        loginHandling(false);
+        document.getElementById("top-menu-line").classList.remove("display-off");
+    });
+
+    document.getElementById("quit-button").addEventListener("click", function () {
+        modalHandling();
+        loginHandling(true);
+    });
 })();
-
-
